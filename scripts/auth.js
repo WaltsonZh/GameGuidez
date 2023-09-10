0a46d7fdaf3c0de355428234f76193d65bdcf73c
adamaconguli <waltson2003@gmail.com>
2023-09-10 20:09:22 +0800

    lesson 6: sign out

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,8 +1,7 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signOut } from 'firebase/auth'
 import { auth } from './firebase.js'
 
 const signupForm = document.querySelector('#signup-form')
-
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const email = signupForm['signup-email'].value
@@ -14,10 +13,18 @@ signupForm.addEventListener('submit', (e) => {
    * admin@admin.com, admin123
    * waltson2003@gmail.com, waltsonZh
    */
-  
+
   createUserWithEmailAndPassword(auth, email, password).then((cred) => {
     const modal = document.querySelector('#modal-signup')
     M.Modal.getInstance(modal).close()
     signupForm.reset()
+  })
+})
+
+const logout = document.querySelector('#logout')
+logout.addEventListener('click', (e) => {
+  e.preventDefault()
+  signOut(auth).then(() => {
+    console.log('user signed out.')
   })
 })
