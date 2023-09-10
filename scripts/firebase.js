e4b6803a34b9601b7d06488d283fde24781ccab6
adamaconguli <waltson2003@gmail.com>
2023-09-10 19:15:42 +0800

    install firebase

---
+++ scripts/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBjHdjCHbnzrpWMN1qDBI8KAmV1Pu7sDd0',
+  authDomain: 'game-guidez-416c3.firebaseapp.com',
+  projectId: 'game-guidez-416c3',
+  storageBucket: 'game-guidez-416c3.appspot.com',
+  messagingSenderId: '646717595986',
+  appId: '1:646717595986:web:a64181298cccb68ea71a8b',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
